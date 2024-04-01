8e5cdbd213d1fb771f537acd8bf1f9f1a0e09a15
EverydayImcalculating <87575444+EverydayImcalculating@users.noreply.github.com>
2024-04-01 14:15:26 +0700

    feat:Add server recv and client send

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+import threading
+
+HEADER = 1024
+FORMAT = "utf-8"
+PORT = 5050
+SERVER = "127.0.0.1"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b" " * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
--- server.py
+++ server.py
@@ -2,10 +2,45 @@ import socket
 import threading
 import os
 
+BUFFER = 1024
+FORMAT = "utf-8"
 PORT = 5050
-SERVER = socket.gethostbyaddr(socket.gethostname())
-ADDR = (SERVER , PORT)
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "127.0.0.1"
+ADDR = (SERVER, PORT)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
+server.listen();
 
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    isConnected = True
+    while isConnected:
+        msg_length = conn.recv(BUFFER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                isConnected = False
+
+            print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
